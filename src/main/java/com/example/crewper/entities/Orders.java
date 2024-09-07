c592ab7aee6f0ab49666548a0efe39dfb3c0d194
VEX <ahmedsherif000035@gmail.com>
2024-09-07 22:30:45 +0300

    add json annotation to handle `LocalDateTime`

--- src/main/java/com/example/crewper/entities/Orders.java
+++ src/main/java/com/example/crewper/entities/Orders.java
@@ -1,11 +1,13 @@
 package com.example.crewper.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
@@ -16,11 +18,12 @@ import java.util.Date;
 @NoArgsConstructor
 @Setter
 @Getter
-public class Orders {
+public class Orders implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime date;
     private String status;
 }
