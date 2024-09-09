6eca596a38eccecc5df773208f9068dbbe53bc47
VEX <ahmedsherif000035@gmail.com>
2024-09-09 14:25:48 +0300

    add OrderStatusDTO to represent the new status of the order

---
+++ src/main/java/com/example/crewper/model/OrderStatusDTO.java
@@ -0,0 +1,14 @@
+package com.example.crewper.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class OrderStatusDTO {
+    private String status;
+}
