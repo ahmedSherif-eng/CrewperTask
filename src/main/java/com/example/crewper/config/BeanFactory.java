9f780e04f505a2a83baa07e8d873dc5fc0b5ef41
VEX <ahmedsherif000035@gmail.com>
2024-09-10 14:49:40 +0300

    Add the objectMapper Bean in BeanFactory

--- src/main/java/com/example/crewper/config/BeanFactory.java
+++ src/main/java/com/example/crewper/config/BeanFactory.java
@@ -1,5 +1,7 @@
 package com.example.crewper.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,5 +11,11 @@ public class BeanFactory {
     @Bean
     public ModelMapper getModelMapper() {
         return new ModelMapper();
+    }
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
     }
 }
